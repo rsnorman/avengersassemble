4447562b37e2cb732bc30bbc2ac9dbcecf67ae6c
Ryan Norman <rsnorman15@gmail.com>
2015-05-13 21:49:32 -0400

    Removed marvel keys from repo

--- app/services/marvel_client.rb
+++ app/services/marvel_client.rb
@@ -1,12 +1,28 @@
+require 'yaml'
+
 class MarvelClient
 
   delegate :characters, to: :@client
 
   def initialize
     @client = Marvelite::API::Client.new(
-      public_key:  '534a1c83ec57591f6de64ea33b87d483',
-      private_key: 'f12aa40d394a82ae7b0ebe08351310e87cf50d9c'
+      public_key:  MarvelClient.public_key,
+      private_key: MarvelClient.private_key
     )
+  end
+
+  private
+
+  def self.public_key
+    ENV['marvel_public_key'] || config['marvel']['public_key']
+  end
+
+  def self.private_key
+    ENV['marvel_public_key'] || config['marvel']['private_key']
+  end
+
+  def self.config
+    @config ||= YAML.load(File.read(Rails.root.join('marvel_keys.yml')))
   end
 
 end
