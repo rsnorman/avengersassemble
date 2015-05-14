9c1dd701b5ac8a4fd60b008db5e7636877d776d6
Ryan Norman <rnorman@covermymeds.com>
2015-05-13 22:55:38 -0400

    Removed unecessary markup

--- app/assets/javascripts/components/app.js.jsx
+++ app/assets/javascripts/components/app.js.jsx
@@ -20,46 +20,6 @@ var App = React.createClass({
             <li><a href="#" className="button">Teams</a></li>
           </ul>
 
-          <div className="row">
-
-            <div className="large-6 columns">
-
-              <img src="http://placehold.it/500x500&text=Image" /><br/>
-
-            </div>
-
-
-          <div className="large-6 columns">
-
-            <h3 className="show-for-small">Header<hr/></h3>
-
-            <div className="panel">
-              <h4 className="hide-for-small">Header<hr/></h4>
-              <h5 className="subheader">Fusce ullamcorper mauris in eros dignissim molestie posuere felis blandit. Aliquam erat volutpat. Mauris ultricies posuere vehicula. Sed sit amet posuere erat. Quisque in ipsum non augue euismod dapibus non et eros. Pellentesque consectetur tempus mi iaculis bibendum. Ut vel dolor sed eros tincidunt volutpat ac eget leo.</h5>
-            </div>
-
-            <div className="row">
-              <div className="large-6 small-6 columns">
-                <div className="panel">
-                  <h5>Header</h5>
-                  <h6 className="subheader">Praesent placerat dui tincidunt elit suscipit sed.</h6>
-                  <a href="#" className="small button">BUTTON TIME!</a>
-                </div>
-              </div>
-
-              <div className="large-6 small-6 columns">
-                <div className="panel">
-                  <h5>Header</h5>
-                  <h6 className="subheader">Praesent placerat dui tincidunt elit suscipit sed.</h6>
-                  <a href="#" className="small button">BUTTON TIME!</a>
-                </div>
-              </div>
-            </div>
-
-          </div>
-
-        </div>
-
         <div className="row">
 
           <div className="large-12 columns">
